6e486e024a3aa2c37b477eca39ca83ec52f164e4
arnulfoacevedo <arnulfoacevedo215@gmail.com>
2024-02-19 11:57:26 -0500

    added multi filters

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -5,21 +5,42 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("salesreportZUSD_REPORT.controller.View1", {
-		// sort flag
 		sortby: true,
 
+		filterKeys: ['RimSize', 'Category', 'MFG', 'Type', 'Line', 'WhlWidth', 'BoltPattern1', 'BoltPattern2', 'TypeCode', 'TypeDesc', 'Width', 'AspectRatio'],
+
 		onInit: function () {
-			this.byId("rundateid").setDateValue(new Date());
-			this.byId("rundateid").setMaxDate(new Date());
+			this.byId("curDate").setDateValue(new Date());
+			this.byId("curDate").setMaxDate(new Date());
 		},
 		onSortByCode: function (e) {
 			this.getData(true);
 			this.sortby = !this.sortby;
-			var label = this.sortby ? "PRODUCT-CODE ↑" : "PRODUCT-CODE ↓";
+			var label = this.sortby ? "Sort by PRODUCT-CODE ↑" : "Sort by PRODUCT-CODE ↓";
 			e.getSource().setText(label);
 		},
+
+		// clearTable: function() {
+		// 	var table 	= this.getView().byId("SF1_TABLE");
+
+		// 	var oModel = new sap.ui.model.json.JSONModel();
+		// 	oModel.setData([]);
+			
+		// 	table.setModel(oModel, "oLocalFinalsalesModel");
+		// },
+
 		// get data by year, month
 		onSearch: function () {
+			this.getData();
+		},
+
+		// get data by year, month
+		onClear: function () {
+			var $this = this;
+			$this.filterKeys.forEach(function(key) {
+				$this.byId(key).setValue('');
+			});
+
 			this.getData();
 		},
 
@@ -29,25 +50,45 @@ sap.ui.define([
 
 		// get data between date range, and then sort the data by product code
 		getData: function (sortby) {
-			var dateRange = this.setTableHeaderByMonth(this.byId('rundateid').getDateValue());
+			var $this 				= this;
+			var oLocalModel 		= this.getView().getModel("localModel");
+			var oLocalData 			= oLocalModel.getData().results;
+			var tempData 			= {};
+			var filterValues 		= {};
+			var selectedDate 		= this.byId('curDate').getDateValue();
+			var dateRange 			= this.setTableHeaderByMonth(selectedDate);
 
-			var oLocalModel = this.getView().getModel("localModel");
-			var oLocalData = oLocalModel.getData().results;
+			var notEmptyNum 		= 0;
+			$this.filterKeys.forEach(function(key) {
+				var value 			= $this.byId(key).getValue();
+				filterValues[key] 	= $.trim(value);
+				if (value != '') notEmptyNum++;
+			});
 
-			var tempData = {};
-			oLocalData.forEach(function (item) {
+			// filter by conditions
+			for (var i = 0; i < oLocalData.length; i++) {
+				var item = oLocalData[i];
+
 				if (dateRange.indexOf(item.Date_YearMonth) > -1) {
+					var num = 0;
+					for (var key in filterValues) {
+						if (filterValues[key] != '' && item[key].indexOf(filterValues[key]) > -1) { 		// item[key] == filterValues[key]
+							num++;
+					   	}
+					}
+					if (num != notEmptyNum && notEmptyNum != 0) continue;
+
 					var uniCode = item['TypeCode'] + '-' + item['TypeDesc'] + '_' + item['ProductSize'];
 					tempData[uniCode] == undefined ? tempData[uniCode] = {} : '';
 					tempData[uniCode][item['Articlecode']] == undefined ? tempData[uniCode][item['Articlecode']] = [] : '';
 					tempData[uniCode][item['Articlecode']].push(item);
 				}
-			});
+			}
 
 			var finalData = [];
 			for (var uniCode in tempData) {
-				var articleCodeArr = tempData[uniCode];
-				var productCode_Size = uniCode.split('_');
+				var articleCodeArr 		= tempData[uniCode];
+				var productCode_Size 	= uniCode.split('_');
 				for (var articleCode in articleCodeArr) {
 					var productMonthItem = {
 						ProductCode: productCode_Size[0],
@@ -71,14 +112,14 @@ sap.ui.define([
 				}
 			}
 
-			var result_sales_data = [];  	// final sales data
-			var totalRow = {};				// temp
-			var grand_total = 0;			// YID grand total
-			var grand_total_month = {};		// grand total by month
+			var totalRow 			= {};	// temp
+			var grand_total 		= 0;	// YID grand total
+			var grand_total_month 	= {};	// grand total by month
+			var result_sales_data 	= [];  	// final sales data
 
 			dateRange.forEach(function (monthStr, monthIndex) {
-				totalRow[monthIndex] = 0;
-				grand_total_month[monthIndex] = 0;
+				totalRow[monthIndex] 			= 0;
+				grand_total_month[monthIndex] 	= 0;
 			});
 
 			for (var i = 0; i < finalData.length; i++) {
@@ -96,9 +137,9 @@ sap.ui.define([
 				// add sub total row
 				if (finalData.length == (i + 1)) {
 					var row = {}, total = 0;
-					row['ArticleCode'] = finalData[i].Articlecode;
-					row['CodeName'] = 'sub total';
-					row['ProductCode'] = finalData[i].ProductCode;
+					row['ArticleCode'] 		= finalData[i].Articlecode;
+					row['CodeName'] 		= 'sub total';
+					row['ProductCode'] 		= finalData[i].ProductCode;
 					dateRange.forEach(function (monthStr, monthIndex) {
 						row[monthIndex] = totalRow[monthIndex] == 0 ? '' : totalRow[monthIndex];
 						grand_total_month[monthIndex] += Number(totalRow[monthIndex]);
@@ -112,9 +153,9 @@ sap.ui.define([
 					var nextProductCode = finalData[i + 1].ProductCode;
 					if (curProductCode != nextProductCode) {
 						var row = {}, total = 0;
-						row['ArticleCode'] = finalData[i].Articlecode;
-						row['CodeName'] = 'sub total';
-						row['ProductCode'] = curProductCode;
+						row['ArticleCode'] 		= finalData[i].Articlecode;
+						row['CodeName'] 		= 'sub total';
+						row['ProductCode'] 		= curProductCode;
 						dateRange.forEach(function (monthStr, monthIndex) {
 							row[monthIndex] = totalRow[monthIndex] == 0 ? '' : totalRow[monthIndex];
 							grand_total_month[monthIndex] += Number(row[monthIndex]);
@@ -132,17 +173,18 @@ sap.ui.define([
 			}
 
 			// add grand total row
-			var row = {};
-			row['ArticleCode'] = 'grand';
-			row['CodeName'] = 'grand total';
-			row['ProductCode'] = '';
+			var row 				= {};
+			row['ArticleCode'] 		= 'grand';
+			row['CodeName'] 		= 'grand total';
+			row['ProductCode'] 		= '';
 			row['year'] = grand_total;
 			dateRange.forEach(function (monthStr, monthIndex) {
 				row[monthIndex] = grand_total_month[monthIndex];
 			});
 
-			var oLocalFinalsalesModel = new JSONModel(result_sales_data);
-			this.getView().setModel(oLocalFinalsalesModel, "oLocalFinalsalesModel");
+			var oLocalFinalsalesModel = new sap.ui.model.json.JSONModel();
+			oLocalFinalsalesModel.setData(result_sales_data);
+			this.getView().byId("SF1_TABLE").setModel(oLocalFinalsalesModel, "oLocalFinalsalesModel");
 
 			// dataModel for grand total
 			var oLocalGrandModel = new JSONModel([row]);
@@ -151,15 +193,15 @@ sap.ui.define([
 
 		// set dynamic header of table, and return array of ids
 		setTableHeaderByMonth: function (sDate) {
-			var theMonths = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-			var now = new Date(sDate + 1);
+			var theMonths 	= new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+			var now 		= new Date(sDate + 1);
 			var lvMonthYear = [];
 
 			var no = 1;
 			for (var i = 0; i > -12; i--) {
-				var future = new Date(now.getFullYear(), now.getMonth() + i, 1);
-				var month = theMonths[future.getMonth()];
-				var year = future.getFullYear();
+				var future 	= new Date(now.getFullYear(), now.getMonth() + i, 1);
+				var month 	= theMonths[future.getMonth()];
+				var year 	= future.getFullYear();
 				lvMonthYear.push(String(year) + String(this.pad(future.getMonth() + 1, 2)));
 
 				this.byId("SF2_Head_Month_" + no).setText(month + '-' + String(year).slice(2));
